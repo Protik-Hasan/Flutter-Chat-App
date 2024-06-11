90d2479dcd35ec92e3a2312e121ff201a14c903a
Protik <2022133@iub.edu.bd>
2024-06-11 16:28:32 +0600

    map generated

--- lib/discover_page.dart
+++ lib/discover_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DiscoverPage extends StatefulWidget {
   @override
@@ -8,34 +6,7 @@ class DiscoverPage extends StatefulWidget {
 }
 
 class _DiscoverPageState extends State<DiscoverPage> {
-  GoogleMapController? _mapController;
-  bool _showMap = false;
-  Position? _currentPosition;
   bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  _getCurrentLocation() async {
-    setState(() {
-      _isLoading = true;
-    });
-    try {
-      final position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      setState(() {
-        _currentPosition = position;
-        _isLoading = false;
-        _showMap = true; // Add this line to show the map after getting the location
-      });
-    } catch (e) {
-      print('Error: $e');
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +32,7 @@ class _DiscoverPageState extends State<DiscoverPage> {
                     ),
                     SizedBox(height: 8),
                     ElevatedButton(
-                      onPressed: _isLoading? null : _getCurrentLocation, // Add your button press action here
+                      onPressed: () {}, // Remove the onPressed callback
                       child: Row(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -75,25 +46,10 @@ class _DiscoverPageState extends State<DiscoverPage> {
               ],
             ),
           ),
-          _isLoading
-              ? Center(
-            child: CircularProgressIndicator(),
-          )
-              : _showMap
-              ? Expanded(
-            child: GoogleMap(
-              onMapCreated: (GoogleMapController controller) {
-                _mapController = controller;
-              },
-              initialCameraPosition: CameraPosition(
-                target: _currentPosition!= null
-                    ? LatLng(_currentPosition!.latitude, _currentPosition!.longitude)
-                    : LatLng(37.7749, -122.4194),
-                zoom: 12,
-              ),
-            ),
-          )
-              : Container(),
+          // Display a static map image
+          Expanded(
+            child: Image.asset('assets/static_map.jpeg'),
+          ),
         ],
       ),
     );
